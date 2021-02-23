5cb71e626307539145476809ae672204327b5784
Tadhg Downey <ec2-user@ip-172-31-1-93.us-west-1.compute.internal>
2021-02-23 02:57:40 +0000

    Added bad code to see linter fail

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ app = Flask(__name__)
 def hello():
     return "<h3>Hello, Udacity! Welcome to my Capstone project.<h3>"
 
+var=
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80, debug=True) # Run on port 80
