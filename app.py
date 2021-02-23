096b52a7601051f71ddbd2c3caecf69fa59ffb93
Tadhg Downey <ec2-user@ip-172-31-1-93.us-west-1.compute.internal>
2021-02-23 03:02:12 +0000

    Removed linter breaking test, added comment to app.py

--- app.py
+++ app.py
@@ -3,12 +3,10 @@ from flask import Flask
 
 app = Flask(__name__)
 
-
+# Homepage
 @app.route('/')
 def hello():
     return "<h3>Hello, Udacity! Welcome to my Capstone project.<h3>"
-
-var=
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80, debug=True) # Run on port 80
